1d33ac988bbae94cafd20d5bf422bb2dfb7be626
Brian Eng <brian@brianeng.com>
2014-03-01 14:20:57 -0600

    add the hello project

---
+++ projects/hello/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = '87f581b17296c7ce0c8b306af2812e499600a20a5a3db2f4726754d7545a0d7b0a3f3aae2bac5829f900516643482c6c6627d693d13e15b9085a22bed0284954'
---
+++ projects/hello/config/routes.rb
@@ -0,0 +1,3 @@
+Hello::Application.routes.draw do
+  get '/hello' => 'hello/index'
+end
